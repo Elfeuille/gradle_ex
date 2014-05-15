0c3af7b68dc58e77e3a2a46a0c8d22747db615de
Hiroki.Ito (A12394) <ito_hiroki@cyberagent.co.jp>
2014-05-15 11:41:51 +0900

    Java-based Configuration

---
+++ gradle_ex_core/src/main/java/jp/elfeuille/gradle/ex/core/config/DefaultConfiguration.java
@@ -0,0 +1,10 @@
+package jp.elfeuille.gradle.ex.core.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "jp.elfeuille.gradle.ex")
+public class DefaultConfiguration {
+
+}
